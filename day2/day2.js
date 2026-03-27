07033c1ea39de04bb1d966066a7967fb4728fb01
Muawiaimtiaz1 <muawiaimtiaz@gmail.com>
2026-03-27 18:04:22 +0500

    day 2 practice

---
+++ day2/day2.js
@@ -0,0 +1,35 @@
+// const arr=[1,2,3,4,5]
+// console.log(arr)
+
+// const arr=[1,2,3,4,5]
+// console.log(arr.length)
+
+// const arr=[1,2,3,4,5]
+// console.log("first element :",arr[0])
+// console.log("last element  :",arr.at(-1))
+
+// const arr=[1,2,3,4,5]
+// console.log("adding at start")
+// arr.unshift(0)
+// console.log("adding element at the end")
+// arr.push(6)
+// //new_arr=[...arr,6]
+// console.log(arr)
+// const flowers=["red","blue","lilly"]
+// console.log("removing from start and end of an array",flowers)
+// flowers.shift()
+// console.log("removed first element",flowers)
+// flowers.pop()
+// console.log("removed last element",flowers)
+
+// const arr=[1,2,3,4,5]
+// key=4
+// for(let i=0;i<arr.length;i++){ 
+//     if (key===arr[i]){
+//        console.log(key," is at index ",i)
+
+
+//     }
+    
+
+// }
